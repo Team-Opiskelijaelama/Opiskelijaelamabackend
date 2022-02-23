aef63ba89336aefcef459f91400d38cf9268f919
Maruzella <mari.h.rautanen@gmail.com>
2022-02-23 12:32:46 +0200

    kokeilu herokua varten

--- src/main/java/example/com/opiskelijaelama/OpiskelijaelamaApplication.java
+++ src/main/java/example/com/opiskelijaelama/OpiskelijaelamaApplication.java
@@ -1,7 +1,6 @@
 package example.com.opiskelijaelama;
 
 
-import java.util.List;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -50,7 +49,7 @@ public class OpiskelijaelamaApplication {
 			Juomapeli peli2 = new Juomapeli("En ole koskaan", "Jokainen pelaaja kertoo vuorollaan asian, jota ei ole tehnyt koskaan. Ne pelaajista, jotka ovat tehneet kyseisen asian, ottavat huikan.");
 			Juomapeli peli3 = new Juomapeli("Fizz Buzz", "Pelaajat istuvat ringissä. Ensimmäinen pelaaja aloittaa sanomalla luvun ”1”. Tämän jälkeen pelaajat luettelevat vuorotellen lukuja ykkösestä ylöspäin. Kaikki luvut, jotka ovat kolmella jaollisia korvataan sanalla Fizz. Kaikki luvut, jotka ovat jaollisia luvulla 5 korvataan sanalla Buzz. Jos luku on jaollinen sekä kolmella että viidellä, sanotaan Fizz Buzz. Jos pelaaja epäröi tai tekee virheen, niin hän joutuu juomaan.");
 			
-			lauluRepo.save(new Laulu("Helan går", "En liten fågel satt en gång och sjöng i furuskog."
+			Laulu laulu1 = new Laulu("Helan går", "En liten fågel satt en gång och sjöng i furuskog."
 					+ "\n"
 					+ " Den hade sjungit dagen lång, men dock ej sjungit nog."
 					+ "\r \n"
@@ -64,10 +63,12 @@ public class OpiskelijaelamaApplication {
 					+ "\n"
 					+ "Helan går- "
 					+ "\n"
-					+ "sjung hopp-fa-de-ral-lan-lej!", "","", true));		
-			lauluRepo.save(new Laulu("Vesipoikien marssi", "Pois se meistä, että täällä maisteltais`\nNoita pahan tuomia alkoholijuomia.\nVaikka henki keltä täällä haisteltais\nNiin huugo raikas sois\n\nHurraa, me nuoret vesipojat,\nPää on selvä meillä aina,\nKrapulat ei meitä paina.\nHurraa, me nuoret vesipojat,\nmeillä luonto raitis on!\n\nSäyseästi näin kun aina elelee,\nItseksensä hissuksiin,\nnenä kirjassansa kiinn\nTarmokkaasti lasiin aina sylkäisee,\nSiitä riemu verraton!\n\nHurraa, me nuoret...\n\nPaha perii riettauden sellaisen,\nJota toiset harjoittaa,\nKun he itsens ́ juovuttaa.\nSukukunnan turma tuopi tuollainen,\nkulttuurin turmion!\n\nHurraa, me nuoret... ", "Kapteeni katsoi horisonttihin", "Ennen laulua, sen aikana ja sen jälkeen skoolatessa saa juoda vain ja ainostaan vettä. Alkoholin juominen laulun aika on rangaistava teko.", true));
-			lauluRepo.save(new Laulu("Jallutähden alla", "Täällä jallutähden alla\nkorkeimmalla kukkulalla\nKatson läpi lasin tyhjän,\nsen täytän uudestaan\n\nTäällä jallutähden alla,\nlasi täyttyy leikatulla\nSiitä suojakseni peiton,\nminä itselleni saan\n\nJa alla jallutähden,\nminä otan, yhden tähden,\nja vain jallutähden nähden,\nitken ilon kyyneleen\n\nTäällä jallutähden alla,\nkova jano laulajalla\nHuolet viinaa naukkaamalla\nsiirtyy päivään huomiseen\n\nTäällä jallutähden alla,\nhiipii sieluun asti halla,\nmutta korkin avaamalla,\nsulaa sydän uudelleen\n\nJa alla jallutähden... ", "Täällä Pohjantähden alla ", "Jallutähden alla lauletaan vain toastmasterin esityksestä ja se on perinteisesti sitsien toiseksi viimeinen laulu. Kappaleen aikana EI huudeta, eikä ennen kappaletta oteta huikkia vaan se lähtee aina hiljaisuudesta. Viimeisen kertosäkeen ajaksi noustaan seisomaan ja se lauletaan kahdesti", true));
+					+ "sjung hopp-fa-de-ral-lan-lej!", "","", true);		
+			Laulu laulu2 = new Laulu("Vesipoikien marssi", "Pois se meistä, että täällä maisteltais`\nNoita pahan tuomia alkoholijuomia.\nVaikka henki keltä täällä haisteltais\nNiin huugo raikas sois\n\nHurraa, me nuoret vesipojat,\nPää on selvä meillä aina,\nKrapulat ei meitä paina.\nHurraa, me nuoret vesipojat,\nmeillä luonto raitis on!\n\nSäyseästi näin kun aina elelee,\nItseksensä hissuksiin,\nnenä kirjassansa kiinn\nTarmokkaasti lasiin aina sylkäisee,\nSiitä riemu verraton!\n\nHurraa, me nuoret...\n\nPaha perii riettauden sellaisen,\nJota toiset harjoittaa,\nKun he itsens ́ juovuttaa.\nSukukunnan turma tuopi tuollainen,\nkulttuurin turmion!\n\nHurraa, me nuoret... ", "Kapteeni katsoi horisonttihin", "Ennen laulua, sen aikana ja sen jälkeen skoolatessa saa juoda vain ja ainostaan vettä. Alkoholin juominen laulun aika on rangaistava teko.", true);
+			Laulu laulu3 = new Laulu("Jallutähden alla", "Täällä jallutähden alla\nkorkeimmalla kukkulalla\nKatson läpi lasin tyhjän,\nsen täytän uudestaan\n\nTäällä jallutähden alla,\nlasi täyttyy leikatulla\nSiitä suojakseni peiton,\nminä itselleni saan\n\nJa alla jallutähden,\nminä otan, yhden tähden,\nja vain jallutähden nähden,\nitken ilon kyyneleen\n\nTäällä jallutähden alla,\nkova jano laulajalla\nHuolet viinaa naukkaamalla\nsiirtyy päivään huomiseen\n\nTäällä jallutähden alla,\nhiipii sieluun asti halla,\nmutta korkin avaamalla,\nsulaa sydän uudelleen\n\nJa alla jallutähden... ", "Täällä Pohjantähden alla ", "Jallutähden alla lauletaan vain toastmasterin esityksestä ja se on perinteisesti sitsien toiseksi viimeinen laulu. Kappaleen aikana EI huudeta, eikä ennen kappaletta oteta huikkia vaan se lähtee aina hiljaisuudesta. Viimeisen kertosäkeen ajaksi noustaan seisomaan ja se lauletaan kahdesti", true);
 
+			
+			
 			tapahtumaRepo.save(sitsit1);
 			tapahtumaRepo.save(rastikierros1);
 			tapahtumaRepo.save(gambinakokous1);
@@ -79,6 +80,9 @@ public class OpiskelijaelamaApplication {
 			peliRepo.save(peli1);
 			peliRepo.save(peli2);
 			peliRepo.save(peli3);
+			lauluRepo.save(laulu1);
+			lauluRepo.save(laulu2);
+			lauluRepo.save(laulu3);
 		};
 	}
 
